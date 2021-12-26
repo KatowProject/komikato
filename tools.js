34770e14e046ac70b793a8e467e79a923a58b2cd
KatowProject <naufalfaqih443@gmail.com>
2021-12-26 12:45:23 +0700

    update

--- tools.js
+++ tools.js
@@ -2,10 +2,6 @@ const PDFDocument = require('pdfkit');
 const getStream = require('get-stream');
 const cheerio = require('cheerio');
 const got = require('got-scraping').gotScraping;
-require('chromedriver');
-const chrome = require('selenium-webdriver/chrome');
-const { Builder, until, By, Key, Capabilities } = require('selenium-webdriver');
-
 
 module.exports = {
     get: (url, option = {}) => {
@@ -15,27 +11,9 @@ module.exports = {
                 //get status code
                 const statusCode = response.statusCode;
                 if (statusCode === 503) {
-                    const driver = new Builder().forBrowser('chrome').build();
+                    const response = await got.get('https://mirror.katowproject.ink/?url=' + url, option);
 
-                    await driver.get(url);
-                    const title = await driver.getTitle();
-                    if (url.includes('wp-json')) {
-                        // find element pre
-                        const pre = await driver.findElement(By.css('pre'));
-                        const html = await pre.getAttribute('innerHTML');
-
-                        resolve({ success: true, body: html });
-                    } else if (url.includes('komikindo.id')) {
-                        await driver.wait(until.titleContains('KomikIndo'), 5000);
-                    } else {
-                        await driver.wait(until.elementLocated(By.css('title')), 5000);
-                    }
-
-                    //get page source
-                    const html = await driver.getPageSource();
-                    await driver.quit();
-
-                    return resolve({ body: html, statusCode: '200' });
+                    return resolve(response);
                 }
                 return resolve(response);
             } catch (e) {
